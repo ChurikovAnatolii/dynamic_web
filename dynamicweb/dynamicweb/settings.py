fed096751e7b4d3e2b02c3f5429a0c70dfb54086
Kazay <kazay@mail.ru>
2023-03-30 18:43:24 +0300

    django work

--- dynamicweb/dynamicweb/settings.py
+++ dynamicweb/dynamicweb/settings.py
@@ -123,3 +123,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['192.168.2.131', 'localhost', '127.0.0.1']
